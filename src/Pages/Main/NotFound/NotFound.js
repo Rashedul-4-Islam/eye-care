2302de100d494f5bbf1a72a7646df68437a8e2d4
Rashedul Islame <rasheduzzaman521@gmail.comm>
2021-10-19 21:46:51 +0600

    responsive

--- src/Pages/Main/NotFound/NotFound.js
+++ src/Pages/Main/NotFound/NotFound.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './NotFound.css'
 
 const NotFound = () => {
     return (
         <div>
-            <img src="https://support.start.me/hc/article_attachments/360001139105/404_page_not_found.jpeg" alt="" />
+            <img className="not-found" src="https://support.start.me/hc/article_attachments/360001139105/404_page_not_found.jpeg" alt="" />
             <div className="mb-5">
             <Link to="/homes" className="btn btn-primary fw-bold text-light mt-3 px-5">Home</Link>
             </div>
